b92d9d7f9601fa55da36e5226848b64985913bf9
CassiaValverde <126363351+CassiaValverde@users.noreply.github.com>
2024-10-16 17:42:44 -0300

    Add files via upload

---
+++ listaDeExercicio21.py
@@ -0,0 +1,5 @@
+# calcular a diferença entre números
+a = float(input("Informe o primeiro número "))
+b = float(input("Informe o segundo número "))
+c = (a-b)*-1
+print (f"A diferença entre os numeros é {c}")
